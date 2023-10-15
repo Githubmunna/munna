75d17a5539f18d28bec3f2313987a8c18cf50cf7
Githubmunna <147505742+Githubmunna@users.noreply.github.com>
2023-10-16 01:52:35 +0530

    the website

---
+++ cms/wp-content/themes/firststepmain/js/common.js
@@ -0,0 +1,46 @@
+/* Smooth-scroll*/
+smoothScroll.init({
+    selector: '.data-scroll', // Selector for links (must be a class, ID, data attribute, or element tag)
+    selectorHeader: null, // Selector for fixed headers (must be a valid CSS selector) [optional]
+    speed: 500, // Integer. How fast to complete the scroll in milliseconds
+    easing: 'easeInOutCubic', // Easing pattern to use
+    offset: 0, // Integer. How far to offset the scrolling anchor location in pixels
+    callback: function ( anchor, toggle ) {} // Function to run after scrolling
+});
+
+$(document).ready(function () {
+	/* Goto Top*/
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+    $('.scrollup').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+
+	/* Registration toggle */	
+	$('.nav-toggle').click(function(){
+			//get collapse content selector
+			var collapse_content_selector = $(this).attr('href');
+
+			//make the collapse content to be shown or hide
+			var toggle_switch = $(this);
+			$(collapse_content_selector).toggle(function(){
+			  if($(this).css('display')=='none'){
+                                //change the button label to be 'Show'
+				toggle_switch.html('Sign Up');
+			  }else{
+                                //change the button label to be 'Hide'
+				toggle_switch.html('Close');
+			  }
+			});
+		  });
+	
+});
